985bc05f0bcab76b987d4c149b7bbf81c615fc2b
MichellyyLima <michellyaugustasilvadelima@gmail.com>
2024-08-26 11:34:30 -0300

    cena1 26.08

--- Meu Game 3D/Assets/slar/SCRIPTS/Player.cs
+++ Meu Game 3D/Assets/slar/SCRIPTS/Player.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 10;
+    public int forcaPulo = 7;
     public Rigidbody rb;
     
     // Start is called before the first frame update
@@ -24,5 +26,16 @@ public class Player : MonoBehaviour
 
          UnityEngine.Vector3 direcao = new Vector3(x,0,y);
          rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+             rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+         }
+         if (transform.position.y < -5)
+         {
+             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+         }
     }
+    
 }
+
