7b705914e2c8a78fd3d8901ea584262bf083b816
MichellyyLima <michellyaugustasilvadelima@gmail.com>
2024-08-28 16:15:26 -0300

    fase1 p.6 28.08

---
+++ Meu Game 3D/Assets/slar/SCRIPTS/COIN.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class COIN : MonoBehaviour
+{
+
+    public int velocidadedeGiro = 60;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().SubtrairMoedas(valor:1);
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidadedeGiro * Time.deltaTime,Space.World);
+    }
+}
